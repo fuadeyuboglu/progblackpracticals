f4983e2afb092751bdb057a65a1e26b86788dccf
BF Eyuboglu <114880492+fuadeyuboglu@users.noreply.github.com>
2025-12-11 11:53:01 +0000

    Change main route message; add about route

--- node_routing/hello.js
+++ node_routing/hello.js
@@ -2,7 +2,11 @@ const express = require('express')
 const app = express()
 
 app.get('/', function(req, resp){
-   resp.send('Hello world')
+   resp.send('Welcome to the homepage!')
+})
+
+app.get('/about', function(req, resp){
+    resp.send('This is the about page for my cool application!')
 })
 
 app.listen(8090)
